1f6edb63e57094faab36336123e8fed8df57a3de
harshira18 <106469219+harshira18@users.noreply.github.com>
2026-07-31 22:42:43 +0530

    Create Brute force 1 - Nested for loops.py

---
+++ 4) 1. Two Sum/Brute force 1 - Nested for loops.py
@@ -0,0 +1,11 @@
+# Time complexity : O(n^2)
+# Space complexity : O(1)
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for i in range(0, len(nums)):
+            for j in range(0, len(nums)):
+                if(i!=j and nums[i]+nums[j]==target):
+                    return [i, j]
+        
+        return []
